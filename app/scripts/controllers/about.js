9d0e9d2d63fa965b6f3be1ff1e6b35e8a1926d71
Xavier Jodoin <xavier@jodoin.me>
2015-11-27 09:09:54 -0500

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pboxWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pboxWebApp
+ */
+angular.module('pboxWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pboxWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pboxWebApp
+ */
+angular.module('pboxWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
