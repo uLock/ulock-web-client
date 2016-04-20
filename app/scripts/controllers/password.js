a1eda37c566773d78f67df42ec52bed918b8af29
Xavier Jodoin <xavier@jodoin.me>
2016-04-20 16:53:31 -0400

    auto add logo

--- app/scripts/controllers/password.js
+++ app/scripts/controllers/password.js
@@ -8,26 +8,32 @@
  * Controller of the ulockWebApp
  */
 angular.module('ulockWebApp')
-  .controller('PasswordCtrl', function ($routeParams,$scope,$location,$http,passwords) {
+  .controller('PasswordCtrl', function($routeParams, $scope, $location, $http, passwords) {
     var passwordId = $routeParams.id;
     var isNew = passwordId === 'new';
 
-    if(isNew) {
+    if (isNew) {
       $scope.pass = {
-        data:{}
+        data: {}
       };
-    }
-    else {
+    } else {
       //load
-      passwords.get(passwordId,function(pass) {
+      passwords.get(passwordId, function(pass) {
         $scope.pass = pass;
       });
     }
 
-    $scope.save = function (pass) {
-      passwords.save(pass,function(){
+    $scope.save = function(pass) {
+      if(pass.website) {
+        pass.logo = '//logo.clearbit.com/'+pass.website+'?size=50';
+      }
+      passwords.save(pass, function() {
         $location.path('/passwords');
       });
+    };
+
+    $scope.cancel = function() {
+      $location.path('/passwords');
     };
 
   });
