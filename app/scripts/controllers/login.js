fbf9f8c9c3cf6518c4971b1f8693574e238b2694
Xavier Jodoin <xavier@jodoin.me>
2016-04-21 16:58:27 -0400

    upgrade patternfly

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,8 +10,6 @@
 angular.module('ulockWebApp')
   .controller('LoginCtrl', function($scope, locker, $location) {
 
-    
-
     $scope.loading = true;
     locker.automaticDecrypt(function(success) {
       if (success) {
