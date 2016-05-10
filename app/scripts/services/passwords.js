18a4fffd7cb34f42384742b5c21d1a4f045716d4
Xavier Jodoin <xavier@jodoin.me>
2016-05-10 08:45:11 -0400

    fix decrypt error blocking

--- app/scripts/services/passwords.js
+++ app/scripts/services/passwords.js
@@ -8,48 +8,50 @@
  * Service in the ulockWebApp.
  */
 angular.module('ulockWebApp')
-  .service('passwords', function ($http,configuration,locker) {
+  .service('passwords', function($http, configuration, locker) {
 
     var decryptResponse = function(callback) {
       return function(res) {
         var password = res.data;
-        if(password) {
+        if (password) {
           var decrypt = locker.decryptEntity(password);
           callback(decrypt);
-        }
-        else{
+        } else {
           callback();
         }
       };
     };
 
-    this.get = function (id, callback) {
-      $http.get(configuration.ulockApi+'/passwords/'+id).then(decryptResponse(callback));
+    this.get = function(id, callback) {
+      $http.get(configuration.ulockApi + '/passwords/' + id).then(decryptResponse(callback));
     };
 
-    this.save = function (pass,callback) {
-      if(pass.id) {
-        $http.put(configuration.ulockApi+'/passwords/'+pass.id,locker.encryptEntity(pass)).then(decryptResponse(callback));
-      }
-      else {
-        $http.post(configuration.ulockApi+'/passwords',locker.encryptEntity(pass)).then(decryptResponse(callback));
+    this.save = function(pass, callback) {
+      if (pass.id) {
+        $http.put(configuration.ulockApi + '/passwords/' + pass.id, locker.encryptEntity(pass)).then(decryptResponse(callback));
+      } else {
+        $http.post(configuration.ulockApi + '/passwords', locker.encryptEntity(pass)).then(decryptResponse(callback));
       }
     };
 
     this.list = function(callback) {
-        $http.get(configuration.ulockApi+'/passwords').then(function (res) {
-          var encryptedPasswords = res.data;
-          if(encryptedPasswords && encryptedPasswords.length > 0) {
-              var passwords = _.map(encryptedPasswords,function (pass) {
-                return locker.decryptEntity(pass);
-              });
-              callback(passwords);
-          }
-          else {
-            callback();
-          }
+      $http.get(configuration.ulockApi + '/passwords').then(function(res) {
+        var encryptedPasswords = res.data;
+        if (encryptedPasswords && encryptedPasswords.length > 0) {
+          var passwords = [];
+          _.each(encryptedPasswords, function(pass) {
+            try {
+               passwords.push(locker.decryptEntity(pass));
+            } catch (ex) {
+              console.error('cannot decrypt entity ' + pass.id);
+            }
+          });
+          callback(passwords);
+        } else {
+          callback();
+        }
 
-        });
+      });
     };
 
   });
