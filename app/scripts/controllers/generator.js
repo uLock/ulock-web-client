e095ea94b4c3bcb513ac41839e444cf2bc85072e
Xavier Jodoin <xavier@jodoin.me>
2016-09-22 16:50:07 -0400

    add password generator

--- app/scripts/controllers/generator.js
+++ app/scripts/controllers/generator.js
@@ -8,10 +8,12 @@
  * Controller of the ulockWebApp
  */
 angular.module('ulockWebApp')
-  .controller('GeneratorCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('GeneratorCtrl', function ($scope) {
+    $scope.passwordlength = 20;
+    $scope.generate = function () {
+      $scope.newPassword = generatePassword($scope.passwordlength,false);
+    };
+
+    $scope.generate();
+
   });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -54,6 +54,7 @@ module.exports = function(config) {
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-patternfly/dist/angular-patternfly.js',
       'bower_components/async/dist/async.js',
+      'bower_components/password-generator/lib/password-generator.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
